ac91206721c0a50369029c28599240fff0c7cc38
Salman Dada <32588710+SalmanDada@users.noreply.github.com>
2021-10-07 12:16:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
+let number = document.getElementById("number");
+let decimal = document.getElementById("decimal");
+let result = document.getElementById("result");
 
+
+
+let add = function add(){
+  result.innerHTML += number;
+}
